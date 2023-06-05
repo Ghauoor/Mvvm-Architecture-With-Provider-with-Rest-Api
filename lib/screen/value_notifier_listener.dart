7698d9e25119a5b55dc8a830e08aa3e606926f63
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-06 01:40:09 +0500

    Use of  ValueListenableBuilder

--- lib/screen/value_notifier_listener.dart
+++ lib/screen/value_notifier_listener.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ValueNotifierListener extends StatelessWidget {
   ValueNotifierListener({super.key});
   final ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  final ValueNotifier<bool> _toogle = ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
@@ -10,28 +11,53 @@ class ValueNotifierListener extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Use Notifier Listner'),
       ),
-      body: Center(
-        child: ValueListenableBuilder(
-          valueListenable: _counter,
-          builder: (context, value, child) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  _counter.value.toString(),
-                  style: const TextStyle(fontSize: 50),
-                ),
-                const SizedBox(height: 10.0),
-                ElevatedButton(
-                    onPressed: () {
-                      _counter.value = 0;
-                    },
-                    child: const Text('Reset'))
-              ],
-            );
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ValueListenableBuilder(
+              valueListenable: _toogle,
+              builder: (context, value, child) {
+                return TextFormField(
+                  obscureText: _toogle.value,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        _toogle.value = !_toogle.value;
+                      },
+                      child: Icon(_toogle.value
+                          ? Icons.visibility
+                          : Icons.visibility_off),
+                    ),
+                  ),
+                );
+              }),
+          const SizedBox(height: 10.0),
+          Center(
+            child: ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context, value, child) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _counter.value.toString(),
+                      style: const TextStyle(fontSize: 50),
+                    ),
+                    const SizedBox(height: 10.0),
+                    ElevatedButton(
+                        onPressed: () {
+                          _counter.value = 0;
+                        },
+                        child: const Text('Reset'))
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
